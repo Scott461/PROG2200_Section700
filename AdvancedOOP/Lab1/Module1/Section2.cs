cf9e34fad308b6b04d24bc47e27fb1ee95f6e275
Scott461 <55257861+Scott461@users.noreply.github.com>
2021-09-26 15:19:05 -0300

    code for section 2

--- AdvancedOOP/Lab1/Module1/Section2.cs
+++ AdvancedOOP/Lab1/Module1/Section2.cs
@@ -4,7 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1.Module1 {
-    class Class1 {
+namespace Section2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("What is the pass code?");
+            var code = Console.ReadLine();
+
+            if (code == "secret") {
+                Console.WriteLine("Authenticated");
+                Console.WriteLine("Do you want to enter a new password? Y/N");
+                var answer = Console.ReadLine();
+                if (answer == "Y") {
+                    Console.WriteLine("Enter new password");
+                    var newPassword = Console.ReadLine();
+                    // In reality, you would need to save the user's NEW pw to disc or database 
+                } 
+            } else if (code != "secret") {
+                Console.WriteLine("Not Authenticated");
+            }
+
+        }
     }
 }
+
+
